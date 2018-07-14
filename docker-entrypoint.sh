3dc929cc170ddebd0588c637b7c39844fec07d59
Matt Light <matt@paren.io>
2018-07-13 19:44:57 -0700

    Add ability to pass extra CLI options to Jekyll
    Set JEKYLL_CLI_OPTS environment variable with options such as
    `--incremental`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [[ $# -eq 0 ]]; then
-    exec jekyll serve \
-        --host 0.0.0.0 \
-        --port "${JEKYLL_PORT:-80}" \
-        --source /jekyll/source \
-        --destination /jekyll/destination \
-        --force_polling
+    COMMAND="jekyll serve --host 0.0.0.0 --port ${JEKYLL_PORT:-80}"
+    COMMAND="${COMMAND} --source /jekyll/source --destination /jekyll/destination"
+    COMMAND="${COMMAND} --force_polling ${JEKYLL_CLI_OPTS:-}"
+
+    echo ${COMMAND}
+    exec ${COMMAND}
 fi
 
 exec "$@"
