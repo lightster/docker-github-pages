ee7675e077abe472d937b24ab00e546f2048b363
Matt Light <matt@paren.io>
2019-07-04 21:17:34 -0700

    Use _config.dev.yml if it exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,13 @@
 if [[ $# -eq 0 ]]; then
     COMMAND="jekyll serve --host 0.0.0.0 --port ${JEKYLL_PORT:-80}"
     COMMAND="${COMMAND} --source /jekyll/source --destination /jekyll/destination"
-    COMMAND="${COMMAND} --force_polling ${JEKYLL_CLI_OPTS:-}"
+    COMMAND="${COMMAND} --force_polling"
+
+    if [ -f _config.dev.yml ]; then
+      COMMAND="${COMMAND} --config _config.yml,_config.dev.yml"
+    fi
+
+    COMMAND="${COMMAND} ${JEKYLL_CLI_OPTS:-}"
 
     echo ${COMMAND}
     exec ${COMMAND}
